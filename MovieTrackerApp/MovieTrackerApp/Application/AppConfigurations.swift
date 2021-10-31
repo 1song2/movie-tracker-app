574958dafe3cee5a7ae3f2386f5b8d3728bb198c
1song2 <1song2918@gmail.com>
2021-11-01 02:16:05 +0900

    chore(network): set API base URL (#3)

---
+++ MovieTrackerApp/MovieTrackerApp/Application/AppConfigurations.swift
@@ -0,0 +1,17 @@
+//
+//  AppConfigurations.swift
+//  MovieTrackerApp
+//
+//  Created by Song on 2021/11/01.
+//
+
+import Foundation
+
+final class AppConfiguration {
+    lazy var apiBaseURL: String = {
+        guard let apiBaseURL = Bundle.main.object(forInfoDictionaryKey: "ApiBaseURL") as? String else {
+            fatalError("ApiBaseURL must not be empty in plist")
+        }
+        return apiBaseURL
+    }()
+}
