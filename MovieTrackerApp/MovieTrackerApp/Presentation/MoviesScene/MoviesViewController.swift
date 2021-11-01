f86df637c90cb129dd39e9658e3ce422828972c1
1song2 <1song2918@gmail.com>
2021-11-02 00:43:17 +0900

    feat(view): add right bar button item to record movies (#10)

--- MovieTrackerApp/MovieTrackerApp/Presentation/MoviesScene/MoviesViewController.swift
+++ MovieTrackerApp/MovieTrackerApp/Presentation/MoviesScene/MoviesViewController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class MoviesViewController: UIViewController {
+    private let disposeBag = DisposeBag()
+    
     static func create() -> MoviesViewController {
         return MoviesViewController()
     }
@@ -15,5 +19,10 @@ class MoviesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "나의 영화 노트" // viewModel.screenTitle
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+        navigationItem.rightBarButtonItem?.rx.tap
+            .subscribe(onNext: {
+                print("button tapped")
+            }).disposed(by: disposeBag)
     }
 }
