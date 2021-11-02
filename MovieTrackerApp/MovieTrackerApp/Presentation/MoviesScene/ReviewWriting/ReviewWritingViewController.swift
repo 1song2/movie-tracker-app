3c8b6f47ce6d4c52f470a35729374f35ed304306
1song2 <1song2918@gmail.com>
2021-11-02 20:18:37 +0900

    feat(view): make done button enabled when text view is not empty (#12)
    - Make it go to root view controller by tapping done button

--- MovieTrackerApp/MovieTrackerApp/Presentation/MoviesScene/ReviewWriting/ReviewWritingViewController.swift
+++ MovieTrackerApp/MovieTrackerApp/Presentation/MoviesScene/ReviewWriting/ReviewWritingViewController.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
+import RxSwift
 
 class ReviewWritingViewController: UIViewController, StoryboardInstantiable {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var doneButton: UIButton!
+    
+    private let disposeBag = DisposeBag()
     
     static func create() -> ReviewWritingViewController {
         let view = ReviewWritingViewController.instantiateViewController()
@@ -19,6 +22,17 @@ class ReviewWritingViewController: UIViewController, StoryboardInstantiable {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
+        
+        textView.rx.text.orEmpty
+            .map { $0.count > 0 }
+            .share(replay: 1)
+            .bind(to: doneButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        doneButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                self?.navigationController?.popToRootViewController(animated: true)
+            }).disposed(by: disposeBag)
     }
     
     private func setupView() {
