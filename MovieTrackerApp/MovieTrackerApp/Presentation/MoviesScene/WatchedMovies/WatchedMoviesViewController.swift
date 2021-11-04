56a5364d6459791b6f26e7dafae8ed204e9bdb72
1song2 <1song2918@gmail.com>
2021-11-04 18:18:43 +0900

    chore: rename all the things related to watchlist to watched movies

--- MovieTrackerApp/MovieTrackerApp/Presentation/MoviesScene/WatchedMovies/WatchedMoviesViewController.swift
+++ MovieTrackerApp/MovieTrackerApp/Presentation/MoviesScene/WatchedMovies/WatchedMoviesViewController.swift
@@ -13,10 +13,12 @@ class WatchedMoviesViewController: UIViewController, StoryboardInstantiable {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var sortingButton: UIButton!
     
+    private var viewModel: WatchedMoviesViewModel!
     private let disposeBag = DisposeBag()
     
-    static func create() -> WatchedMoviesViewController {
+    static func create(with viewModel: WatchedMoviesViewModel) -> WatchedMoviesViewController {
         let view = WatchedMoviesViewController.instantiateViewController()
+        view.viewModel = viewModel
         return view
     }
     
@@ -25,16 +27,16 @@ class WatchedMoviesViewController: UIViewController, StoryboardInstantiable {
         navigationItem.largeTitleDisplayMode = .never
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         
-        let genre = ["영화제목", "영화제목", "영화제목"]
         tableView.dataSource = nil
-        Observable.just(genre).bind(to: self.tableView.rx.items) { _, _, name in
-            let cell = UITableViewCell(style: .value1, reuseIdentifier: "Cell")
-            cell.textLabel?.text = name
-            cell.imageView?.image = UIImage(named: "heart-fill")
-            cell.detailTextLabel?.text = "2020.11.02"
-            cell.accessoryType = .disclosureIndicator
-            return cell
-        }.disposed(by: disposeBag)
+        viewModel.items
+            .bind(to: self.tableView.rx.items) { _, _, item in
+                let cell = UITableViewCell(style: .value1, reuseIdentifier: "Cell")
+                cell.textLabel?.text = item.movie?.title?.htmlEscaped
+                cell.imageView?.image = item.isBookmarked ? UIImage(named: "heart-fill") : UIImage(named: "heart")
+                cell.detailTextLabel?.text = dateFormatter.string(from: item.watchedOn)
+                cell.accessoryType = .disclosureIndicator
+                return cell
+            }.disposed(by: disposeBag)
         
         sortingButton.rx.tap
             .subscribe(onNext: {
@@ -44,3 +46,9 @@ class WatchedMoviesViewController: UIViewController, StoryboardInstantiable {
             }).disposed(by: disposeBag)
     }
 }
+
+private let dateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    return formatter
+}()
